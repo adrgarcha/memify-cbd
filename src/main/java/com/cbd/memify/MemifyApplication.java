530b9f61e2b8017b219091a8260cec0d3c9ce0b9
Adrián García Chavero <adrgarcha@alum.us.es>
2024-03-29 12:22:44 +0100

    First commit

---
+++ src/main/java/com/cbd/memify/MemifyApplication.java
@@ -0,0 +1,13 @@
+package com.cbd.memify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbd/memify/MemifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbd.memify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
