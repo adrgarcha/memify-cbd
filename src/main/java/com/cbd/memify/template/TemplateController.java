48789094105df9d84346931df2eb50a5bc5bbd77
Adrián García Chavero <adrgarcha@alum.us.es>
2024-04-04 22:18:56 +0200

    Refactor template API

--- src/main/java/com/cbd/memify/template/TemplateController.java
+++ src/main/java/com/cbd/memify/template/TemplateController.java
@@ -1,13 +1,17 @@
 package com.cbd.memify.template;
 
 import com.cbd.memify.config.JwtService;
+import com.mongodb.client.gridfs.model.GridFSFile;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/templates")
@@ -19,21 +23,47 @@ public class TemplateController {
 
     @GetMapping
     public ResponseEntity<List<Template>> getTemplates() {
-        return templateService.getTemplates();
+        return ResponseEntity.ok(templateService.getTemplates());
     }
 
     @GetMapping("/search")
     public ResponseEntity<byte[]> getTemplateByName(@RequestParam String name) throws IOException {
-        return templateService.getTemplateByName(name);
+
+        GridFSFile gridFsFile = templateService.findFileByName(name);
+        if (Objects.isNull(gridFsFile)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (Objects.isNull(gridFsFile.getMetadata())) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+        String contentType = gridFsFile.getMetadata().getString("_contentType");
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(contentType)).body(templateService.getTemplateByName(name));
     }
 
     @PostMapping
     public ResponseEntity<Template> addTemplate(@RequestHeader("Authorization") String authHeader,
                                                 @RequestPart String name,
                                                 @RequestPart MultipartFile template) throws IOException {
+
+        if (name.isBlank() || template.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        if (templateService.findFileByName(name) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        if (template.getContentType() != null && !template.getContentType().startsWith("image/")) {
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).build();
+        }
+
         String token = authHeader.substring(7);
         String username = jwtService.extractUsername(token);
-        return templateService.addTemplate(username, name, template);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(templateService.addTemplate(username, name, template));
     }
 
 }
--- src/main/java/com/cbd/memify/template/TemplateService.java
+++ src/main/java/com/cbd/memify/template/TemplateService.java
@@ -6,18 +6,13 @@ import com.mongodb.client.gridfs.model.GridFSFile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +20,7 @@ public class TemplateService {
 
     private final GridFsTemplate gridFsTemplate;
 
-    public ResponseEntity<List<Template>> getTemplates() {
+    public List<Template> getTemplates() {
         List<GridFSFile> gridFsFiles = new ArrayList<>();
         List<Template> templates = new ArrayList<>();
 
@@ -40,48 +35,25 @@ public class TemplateService {
             templates.add(template);
         }
 
-        return ResponseEntity.ok(templates);
+        return templates;
     }
 
-    public ResponseEntity<byte[]> getTemplateByName(String name) throws IOException {
-        GridFSFile gridFsFile = findFileByName(name);
-
-        if (Objects.isNull(gridFsFile)) {
-            return ResponseEntity.notFound().build();
-        }
-
-        GridFsResource gridFsImage = gridFsTemplate.getResource(gridFsFile);
-        byte[] image = gridFsImage.getInputStream().readAllBytes();
-
-        return ResponseEntity.ok().contentType(MediaType.valueOf(gridFsImage.getContentType())).body(image);
+    public byte[] getTemplateByName(String name) throws IOException {
+        return gridFsTemplate.getResource(findFileByName(name)).getInputStream().readAllBytes();
     }
 
-    public ResponseEntity<Template> addTemplate(String username, String name, MultipartFile template) throws IOException {
-
-        if (name.isBlank() || template.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-
-        if (findFileByName(name) != null) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
-
-        String contentType = template.getContentType();
-        if (contentType != null && !contentType.startsWith("image/")) {
-            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).build();
-        }
+    public Template addTemplate(String username, String name, MultipartFile template) throws IOException {
 
         DBObject metaData = new BasicDBObject();
         metaData.put("username", username);
         metaData.put("name", name);
 
-        gridFsTemplate.store(template.getInputStream(), name, contentType, metaData);
-        Template newTemplate = Template.builder().username(username).name(name).build();
+        gridFsTemplate.store(template.getInputStream(), name, template.getContentType(), metaData);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(newTemplate);
+        return Template.builder().username(username).name(name).build();
     }
 
-    private GridFSFile findFileByName(String name) {
+    public GridFSFile findFileByName(String name) {
         return gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("metadata.name").is(name)));
     }
 
