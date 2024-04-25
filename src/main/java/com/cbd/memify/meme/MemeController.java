09aaa23057b45384ea8d5b7c939df29d3f720e4f
Adrián García Chavero <adrgarcha@alum.us.es>
2024-04-25 20:28:27 +0200

    Rename user and meme endpoint

--- src/main/java/com/cbd/memify/meme/MemeController.java
+++ src/main/java/com/cbd/memify/meme/MemeController.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/memes")
+@RequestMapping("/api/memes")
 @AllArgsConstructor
 public class MemeController {
 
--- src/main/java/com/cbd/memify/user/UserController.java
+++ src/main/java/com/cbd/memify/user/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 @AllArgsConstructor
 public class UserController {
 
